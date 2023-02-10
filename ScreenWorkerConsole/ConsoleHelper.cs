d8bb5bedb1ce4897bac4e8c9bf641156a9a1bff1
Aki Evans <aki.evans.dev@gmail.com>
2023-02-10 17:34:55 +0300

    Add console executer

---
+++ ScreenWorkerConsole/ConsoleHelper.cs
@@ -0,0 +1,48 @@
+﻿using AE.Core;
+
+using ScreenBase.Display;
+
+namespace ScreenWorkerConsole;
+
+public static class ConsoleHelper
+{
+    public static void Display(DisplaySpan value)
+    {
+        switch (value.Type)
+        {
+            case DisplaySpanType.LineBreak:
+                Console.WriteLine();
+                break;
+            case DisplaySpanType.Property:
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                break;
+            case DisplaySpanType.Function:
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                break;
+            case DisplaySpanType.Value:
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                break;
+            case DisplaySpanType.Text:
+                Console.ForegroundColor = ConsoleColor.Gray;
+                break;
+            case DisplaySpanType.Error:
+                Console.ForegroundColor = ConsoleColor.Red;
+                break;
+            case DisplaySpanType.Color:
+                value.Text = null;
+                break;
+            case DisplaySpanType.Comment:
+                Console.ForegroundColor = ConsoleColor.Green;
+                break;
+        }
+
+        if (!value.Text.IsNull())
+        {
+            Console.Write(value.Text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        foreach (var section in value.Inlines)
+            Display(section);
+    }
+}
--- ScreenWorkerConsole/Program.cs
+++ ScreenWorkerConsole/Program.cs
@@ -1,1 +1,53 @@
-﻿
+﻿using System.Diagnostics;
+
+using AE.Core;
+
+using ScreenBase;
+using ScreenBase.Data.Base;
+using ScreenBase.Display;
+
+using ScreenWindows;
+
+using ScreenWorkerConsole;
+
+string path = null;
+
+if (args.Length == 0 || args[0].IsNull())
+{
+    Console.Write("Input path: ");
+    path = Console.ReadLine() ?? "";
+}
+else
+    path = args[0];
+
+if (path.IsNull() || !File.Exists(path))
+    Console.WriteLine("No path!");
+else
+{
+    var data = DataHelper.Load<ScriptInfo>(path);
+
+    if (!data.IsEmpty())
+    {
+        Console.WriteLine($"Start {data.Name}, press eny key to stop!");
+
+        var hwnd = Process.GetCurrentProcess().MainWindowHandle;
+        var size = WindowsHelper.GetMonitorSize(hwnd); 
+        
+        var worker = new WindowsScreenWorker();
+        worker.Init(size.Width, size.Height);
+
+        var executor = new ScriptExecutor();
+
+        executor.OnMessage += (message, needDisplay) =>
+        {
+            ConsoleHelper.Display(DisplaySpan.Parse(message));
+            Console.WriteLine();
+        };
+
+        executor.Start(data, worker, true);
+
+        Console.ReadLine();
+
+        executor.Stop(true);
+    }
+}
