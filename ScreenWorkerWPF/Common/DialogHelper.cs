aa6b5e1b344ff58387bfccc78f51ab1ade4d6931
Aki Evans <aki.evans.dev@gmail.com>
2023-03-04 20:01:45 +0300

    update

--- ScreenWorkerWPF/Common/DialogHelper.cs
+++ ScreenWorkerWPF/Common/DialogHelper.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Reflection;
+using System.Security.Policy;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,109 +22,175 @@ namespace ScreenWorkerWPF.Common;
 
 internal static class DialogHelper
 {
-    private const string TAGS_URL = "https://api.github.com/repos/AkiEvansDev/AE.ScreenWorker/tags";
-    private const string FILE_URL = "https://github.com/AkiEvansDev/AE.ScreenWorker/releases/download/{0}/ScreenWorkerSetup{0}.msi";
+    private const string RELEASES_URL = "https://api.github.com/repos/AkiEvansDev/AE.ScreenWorker/releases";
     private const string TOKEN = "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";
     private const string USER_AGENT = "ScreenWorker";
+
+    public static bool IsCheckUpdate { get; set; } = false;
 
     public static string GetVersion()
     {
         return Assembly.GetExecutingAssembly().GetName().Version.ToString().TrimEnd('0', '.');
     }
 
-    public async static Task<string> GetLastVersion()
+    public async static void Update(bool fromUser = false)
     {
-        using var client = GetHttpClient();
-        var result = await client.GetAsync(TAGS_URL);
-
-        if (result.StatusCode == HttpStatusCode.OK)
+        var waitDialog = new ContentDialog
         {
-            var json = await result.Content.ReadAsStringAsync();
-            var tags = JsonConvert.DeserializeObject<dynamic>(json) as IEnumerable<dynamic>;
+            Title = "Checking...",
+            Content = "Progress: 0%",
+            PrimaryButtonText = "Cancel",
+            IsPrimaryButtonEnabled = true,
+            IsSecondaryButtonEnabled = false,
+        };
 
-            if (tags.Any())
-                return tags.First().name;
+        if (IsCheckUpdate)
+        {
+            if (fromUser)
+                await waitDialog.ShowAsync();
+
+            return;
         }
 
-        return null;
-    }
+        IsCheckUpdate = true;
 
-    public async static void Update(bool noNewVersionMessage = false)
-    {
-        var version = GetVersion();
-        var lastVersion = await GetLastVersion();
-
-        if (!lastVersion.IsNull() && version != lastVersion)
-        {
-            if (await ShowMessage("Download and install?", $"Update {lastVersion} available!") == ContentDialogResult.Primary)
-            {
-                var url = string.Format(FILE_URL, lastVersion);
-                var temp = Path.Combine(Path.GetTempPath(), $"sw{lastVersion}.msi");
-
-                using var client = GetHttpClient(TimeSpan.FromMinutes(5));
-
-                var canInstall = false;
-                var cancelTokenSource = new CancellationTokenSource();
-                var downloadDialog = new ContentDialog
-                {
-                    Title = "Downloading...",
-                    Content = "Progress: 0%",
-                    PrimaryButtonText = "Install",
-                    SecondaryButtonText = "Cancel",
-                    IsPrimaryButtonEnabled = false,
-                    IsSecondaryButtonEnabled = true,
-                };
-
-                downloadDialog.Opened += async (s, e) =>
-                {
-                    try
-                    {
-                        using var file = new FileStream(temp, FileMode.Create, FileAccess.Write, FileShare.None);
-                        var progress = new Progress<float>(f => downloadDialog.Content = $"Progress: {Math.Round(f * 100)}%");
-
-                        await client.DownloadAsync(url, file, progress, cancelTokenSource.Token);
-                            
-                        canInstall = true;
-                        downloadDialog.IsPrimaryButtonEnabled = true;
-                    }
-                    catch (Exception ex)
-                    {
-                        downloadDialog.Title = "Error";
-                        downloadDialog.Content = ex.Message;
-                        downloadDialog.PrimaryButtonText = "OK";
-                        downloadDialog.IsPrimaryButtonEnabled = true;
-                        downloadDialog.IsSecondaryButtonEnabled = false;
-                    }
-                };
-
-                if (await downloadDialog.ShowAsync(ContentDialogPlacement.Popup) == ContentDialogResult.Primary && canInstall)
-                {
-                    var process = new Process();
-                    process.StartInfo.FileName = temp;
-                    process.StartInfo.UseShellExecute = true;
-
-                    process.Start();
-                    Application.Current.Shutdown();
-                }
-            }
-        }
-        else if (noNewVersionMessage)
-        {
-            ShowError(null, "No update available!");
-        }
-    }
-
-    private static HttpClient GetHttpClient(TimeSpan? timeout = null)
-    {
         var client = new HttpClient
         {
-            Timeout = timeout == null ? TimeSpan.FromSeconds(5) : timeout.Value
+            Timeout = TimeSpan.FromSeconds(5)
         };
 
         client.DefaultRequestHeaders.Add("Authorization", TOKEN);
         client.DefaultRequestHeaders.Add("User-Agent", USER_AGENT);
 
-        return client;
+        var fileUrl = "";
+        var lastVersion = "";
+
+        var progress = new Progress<float>(f => waitDialog.Content = $"Progress: {Math.Round(f * 100)}%");
+        if (fromUser)
+        {
+            waitDialog.Opened += async (s, e) =>
+            {
+                (fileUrl, lastVersion) = await GetFileUrl(client, progress);
+                waitDialog.Hide();
+            };
+
+            if (await waitDialog.ShowAsync() == ContentDialogResult.Primary)
+                return;
+        }
+        else
+        {
+            (fileUrl, lastVersion) = await GetFileUrl(client, progress);
+            waitDialog.Hide();
+        }
+
+        IsCheckUpdate = false;
+
+        if (!fileUrl.IsNull())
+        {
+            if (await ShowMessage("Download and install?", $"Update {lastVersion} available!") != ContentDialogResult.Primary)
+                return;
+
+            var temp = Path.Combine(Path.GetTempPath(), $"sw{lastVersion}.msi");
+
+            var canInstall = false;
+            var cancelTokenSource = new CancellationTokenSource();
+            var downloadDialog = new ContentDialog
+            {
+                Title = "Downloading...",
+                Content = "Progress: 0%",
+                PrimaryButtonText = "Install",
+                SecondaryButtonText = "Cancel",
+                IsPrimaryButtonEnabled = false,
+                IsSecondaryButtonEnabled = true,
+            };
+
+            downloadDialog.Opened += async (s, e) =>
+            {
+                try
+                {
+                    using var file = new FileStream(temp, FileMode.Create, FileAccess.Write, FileShare.None);
+                    var progress = new Progress<float>(f => downloadDialog.Content = $"Progress: {Math.Round(f * 100)}%");
+
+                    client.Timeout = TimeSpan.FromMinutes(5);
+                    await client.DownloadAsync(fileUrl, file, progress, cancelTokenSource.Token);
+
+                    canInstall = true;
+                    downloadDialog.IsPrimaryButtonEnabled = true;
+                }
+                catch (Exception ex)
+                {
+                    downloadDialog.Title = "Error";
+                    downloadDialog.Content = ex.Message;
+                    downloadDialog.PrimaryButtonText = "OK";
+                    downloadDialog.IsPrimaryButtonEnabled = true;
+                    downloadDialog.IsSecondaryButtonEnabled = false;
+                }
+            };
+
+            if (await downloadDialog.ShowAsync(ContentDialogPlacement.Popup) != ContentDialogResult.Primary)
+                return;
+
+            if (canInstall)
+            {
+                var process = new Process();
+                process.StartInfo.FileName = temp;
+                process.StartInfo.UseShellExecute = true;
+
+                process.Start();
+                Application.Current.Shutdown();
+            }
+        }
+        else if (fromUser)
+        {
+            ShowError(null, "No update available!");
+        }
+    }
+
+    private async static Task<(string FileUrl, string LastVersion)> GetFileUrl(HttpClient client, IProgress<float> progress)
+    {
+        progress.Report(0.1f);
+
+        var version = GetVersion();
+        var lastVersion = version;
+        var assetsUrl = "";
+
+        var result = await client.GetAsync(RELEASES_URL);
+        progress.Report(0.4f);
+        if (result.StatusCode == HttpStatusCode.OK)
+        {
+            var json = await result.Content.ReadAsStringAsync();
+            var releases = JsonConvert.DeserializeObject<dynamic>(json) as IEnumerable<dynamic>;
+
+            if (releases.Any())
+            {
+                var releas = releases.First();
+                lastVersion = releas.tag_name;
+                assetsUrl = releas.assets_url;
+
+                progress.Report(0.5f);
+            }
+        }
+
+        string fileUrl = null;
+        if (version != lastVersion)
+        {
+            result = await client.GetAsync(assetsUrl);
+            progress.Report(0.8f);
+            if (result.StatusCode == HttpStatusCode.OK)
+            {
+                var json = await result.Content.ReadAsStringAsync();
+                var assets = JsonConvert.DeserializeObject<dynamic>(json) as IEnumerable<dynamic>;
+
+                if (assets.Any())
+                {
+                    fileUrl = assets.First().browser_download_url;
+                    progress.Report(0.9f);
+                }
+            }
+        }
+        
+        progress.Report(1);
+        return (fileUrl, lastVersion);
     }
 
     public static async void ShowError(string message, string title = "Error!")
